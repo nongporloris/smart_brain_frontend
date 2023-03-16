e2e75718072eb07919ca576f3ff422859f755dd5
unknown <nongporlloris@gmail.com>
2023-03-16 18:41:19 +0700

    change local fetch

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -22,7 +22,7 @@ class Signin extends React.Component{
 	onSubmitSignIn =() =>{
 
 		// console.log(this.state);
-		fetch('http://localhost:3001/signin', {
+		fetch('https://pcn-smart-brain-backend.onrender.com/signin', {
 			method : 'post',
 			headers : {'Content-Type' : 'application/json'},
 			body : JSON.stringify({
